a0be6539468f848c9cf020a9a7c04efcf05665f6
Kenley Arai <me@kenleyarai.com>
2017-08-26 20:50:30 -0700

    Fixed spelling mistakes

--- src/containers/About.js
+++ src/containers/About.js
@@ -20,8 +20,8 @@ export default class About extends React.Component {
                 <div id="about-me">
                     <div id="message"> 
                         <AboutMeBox src={meImage} title={'The Boy'} msg={"Hello! I'm Kenley, and I am a software developer. I am a motivated individual who loves to learn and grow! Alongside Computer Science I enjoy painting portraits with oil paints. Specifically, I enjoy duplicating Rembrandts. My current interest is developing beautiful websites using libraries and frameworks such as ReactJs with Redux or Ruby on Rails!"} />
-                        <AboutMeBox src={lenaImage} title={'The Girl'}  msg={"This is my fiancé! She along being the love of my life she is getting a Ph.D. from Claremont Graduate School. She's pretty impressive, and you can find out more about her when she gets her site up and running! "}/>
-                        <AboutMeBox src={inuImage} title={'The Mastermind'} msg={"This is my dog he is the brains behind our operation. He's the one who creates our five-year and ten-year plans. We pay him with belly rubs and treats!"} />
+                        <AboutMeBox src={lenaImage} title={'The Girl'}  msg={"This is my fiancé! She is the love of my life, and she is getting a Ph.D. from Claremont Graduate School. She's pretty impressive, and you can find out more about her when she gets her site up and running!"}/>
+                        <AboutMeBox src={inuImage} title={'The Mastermind'} msg={"This is my dog! He is the brains behind our operation. He's the one who creates our five-year and ten-year plans. We pay him with belly rubs and treats!"} />
                     </div>
                 </div>
             </div>
