051ef430af3deee1cf05dbf08ea41e7493caf133
Kenley Arai <me@kenleyarai.com>
2017-11-27 13:44:17 -0800

    Removed old gallery

--- src/containers/Projects.js
+++ src/containers/Projects.js
@@ -2,18 +2,16 @@ import React from "react";
 import Logo from "Logo";
 import Navbar from "components/Navbar";
 import Banner from "components/Banner";
-import GalleryApp from "containers/GalleryApp";
 import "containers/projects.css";
 
 export default class About extends React.Component {
-    render() {
-        return (
-            <div id="projects-page">
-                <Navbar activePage='projects'/>
-                <Logo/>
-                <Banner header={"This is what I work on!"}/>
-                <GalleryApp/>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div id="projects-page">
+        <Navbar activePage="projects" />
+        <Logo />
+        <Banner header={"This is what I work on!"} />
+      </div>
+    );
+  }
 }
