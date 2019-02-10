21bf8d5128a46eb528450e427e39cee363c390fa
Kenley Arai <me@kenleyar.ai>
2019-02-10 14:30:08 -0800

    Renamed  addAPICall HOC to addGETCall HOC to be more accurate about what the componenet does

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Deck from 'components/Deck';
 import PageBanner from 'components/PageBanner';
-import addAPICall from 'HOC/addAPICall';
+import addGETCall from 'HOC/addGETCall';
 
 const Home = ({ data }) => (
   <div className={'page'}>
@@ -10,9 +10,9 @@ const Home = ({ data }) => (
   </div>
 );
 
-const HomePage = addAPICall(
+const HomePage = addGETCall(
   Home,
-  'https://api.sheety.co/57263d46-98a1-496d-8274-c912a4fed7212'
+  'https://api.sheety.co/57263d46-98a1-496d-8274-c912a4fed721'
 );
 
 export default HomePage;
