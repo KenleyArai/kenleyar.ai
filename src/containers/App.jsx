62df41778cde3cfb847d92e810cbf67edca777df
Kenley Arai <me@kenleyar.ai>
2019-02-15 14:31:17 -0800

    Changed routing on App
    The routing now lets you add more pages to your array. This allows me to not have to individually add a page to the router. Instead I can just add the page via the `pages` variable

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,14 +1,17 @@
 import Navigation from 'components/Navigation';
 import AppContainer from 'components/styled/AppContainer';
-import HomePage from 'pages/Home';
 import NotFoundPage from 'pages/NotFound';
-import React, { Component } from 'react';
+import Loading from 'components/Loading';
+import React, { Component, lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import uuidv1 from 'uuid/v1';
+
+const HomePage = lazy(() => import('pages/Home'));
 
 const pages = [
   {
     path: '/',
+    component: HomePage,
     text: 'Home',
     key: uuidv1(),
   },
@@ -27,10 +30,14 @@ export default class App extends Component {
           />
 
           {/* Adding each route which resolves to a Page */}
-          <Switch>
-            <Route exact path="/" component={HomePage} />
-            <Route component={NotFoundPage} />
-          </Switch>
+          <Suspense fallback={<Loading />}>
+            <Switch>
+              {pages.map(page => (
+                <Route exact path={page.path} component={page.component} />
+              ))}
+              <Route component={NotFoundPage} />
+            </Switch>
+          </Suspense>
         </AppContainer>
       </Router>
     );
