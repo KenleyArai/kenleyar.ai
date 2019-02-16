ca2edc9e11b92aa20e2903c557156bb068f6e810
Kenley Arai <me@kenleyar.ai>
2019-02-15 16:20:31 -0800

    Fixed bugs with deck
    * Fixed error where the items passed to the Deck did not have a key

--- src/components/Deck.jsx
+++ src/components/Deck.jsx
@@ -4,14 +4,7 @@ import ListCard from 'components/ListCard';
 const Deck = ({ posts }) => {
   return (
     <ul className="deck">
-      {posts.map(post => (
-        <ListCard
-          key={post.unique_id}
-          unique_key={post.unique_id}
-          desc={post.desc}
-          title={post.title}
-        />
-      ))}
+      {posts.map(post => <ListCard key={posts.key} {...post} />)}
     </ul>
   );
 };
--- src/components/ListCard.jsx
+++ src/components/ListCard.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
-const ListCard = ({ title, desc, unique_key }) => {
+const ListCard = ({ title, desc }) => {
   return (
-    <li key={unique_key} id={unique_key}>
-      <h2>{title}</h2>
-      <p>{desc}</p>
-    </li>
+    <React.Fragment>
+      <li>
+        <h2>{title}</h2>
+        <p>{desc}</p>
+      </li>
+    </React.Fragment>
   );
 };
 
