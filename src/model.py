081b17cdd5adce94248a0bfb03413d4c3ddaac15
Kenley Arai <me@kenleyar.ai>
2019-02-18 16:02:08 -0800

    Updated model to ouput a md file string
    The md file is broken down and the model returns the following:
    
      - desc
      - post (in markdown)
      - title

--- src/model.py
+++ src/model.py
@@ -1,17 +1,35 @@
 from flask import json, url_for
+from src.memo import memo
+from functools import wraps
+import re
 
 
 class Model:
     url = 'http://localhost:5000/'
-    data = [{'title': 'Hello, world', 'desc': 'test post', 'key': 'Hello, world'}]
-    posts = {
-        'Hello_world': {
-            'title': 'Hello_world',
-            'post': url + 'static/mdx/Hello_world.mdx'}
-    }
+    posts_path = 'posts/'
 
+    @memo
     def get_posts(self):
         return json.dumps(self.data)
 
+    @memo
     def get_post(self, key):
-        return json.dumps(self.posts[key])
+
+        still_desc = True
+
+        with open(self.posts_path + key + '.md') as f:
+            desc = []
+            post = []
+
+            for line in f.readlines():
+                if still_desc and not line.startswith('#'):
+                    post.append(line)
+                else:
+                    still_desc = not still_desc
+                    desc.append(line)
+
+        return json.dumps({
+            'title': " ".join(re.findall('[A-Z][a-z]*', key)),
+            'post': "".join(post),
+            'desc': "".join(desc)
+        })
