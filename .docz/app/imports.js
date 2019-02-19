02eca7ec71751a28871712c0925209720dffa40f
Kenley Arai <me@kenleyar.ai>
2019-02-18 17:02:50 -0800

    Removed .docz cache files

--- .docz/app/imports.js
+++
@@ -1,6 +0,0 @@
-export const imports = {
-  'docz/Deck.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "docz-deck" */ 'docz/Deck.mdx'),
-  'docz/ListCard.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "docz-list-card" */ 'docz/ListCard.mdx'),
-}
