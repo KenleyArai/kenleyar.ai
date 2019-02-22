37abc1b63afc18736fa6bae16cbc36707d15af2a
Kenley Arai <me@kenleyar.ai>
2019-02-21 17:48:19 -0800

    Refactored Loader component so that it fades in and out properly

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -2,15 +2,21 @@ import React from 'react';
 import DoneLoader from 'components/styled/DoneLoader';
 import StillLoader from 'components/styled/StillLoader';
 import FailedLoader from 'components/styled/FailedLoader';
+import PropTypes from 'prop-types';
 
 const Loading = ({ failed, data }) => {
   if (failed === undefined || failed === null) {
     return <StillLoader />;
   } else if (failed) {
-    return <FailedLoader />;
+    return <FailedLoader data={data} />;
   } else {
     return <DoneLoader />;
   }
+};
+
+Loading.propTypes = {
+  failed: PropTypes.bool,
+  data: PropTypes.string,
 };
 
 export default Loading;
--- src/components/styled/StillLoader.jsx
+++ src/components/styled/StillLoader.jsx
@@ -4,15 +4,13 @@ import { useSpring, animated } from 'react-spring';
 const StillLoader = () => {
   const [toggle, setToggle] = useState(0);
 
-  setInterval(() => {
-    setToggle(toggle === 1 ? 0 : 1);
-  }, 1200);
-
   const props = useSpring({
-    config: { mass: 1, tension: 280, friction: 120 },
-    to: { opacity: toggle === 1 ? 0.1 : 0.9 },
-    from: { opacity: toggle === 1 ? 0.9 : 0.1 },
+    config: { mass: 2, tension: 200, friction: 50 },
+    to: { opacity: toggle === 1 ? 0.1 : 0.99 },
+    from: { opacity: toggle === 1 ? 0.99 : 0.1 },
+    onRest: () => setToggle(toggle === 1 ? 0 : 1),
   });
+
   return (
     <animated.div style={props} className="loader-container">
       Loading...
