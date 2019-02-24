2961fd8b4a350524954f586979dcbfd2e1823cc8
Kenley Arai <me@kenleyar.ai>
2019-02-23 17:25:32 -0800

    Model now reads the files in posts and returns the desc, tile and post via an API

--- src/model.py
+++ src/model.py
@@ -1,7 +1,25 @@
 from flask import json, url_for
 from src.memo import memo
-from functools import wraps
 import re
+import os
+
+
+def clean_filename(filename):
+    res = []
+    # Splitting words based on capitals but we have to clean up the
+    # case where 2 capitals are right next to each other
+    for word in re.sub("([a-z])([A-Z])", "\g<1> \g<2>", filename).split():
+        if len(word) > 1:
+            # checking to see if 2 capital letters are right next to each other
+            if word[0].isupper() and word[1].isupper():
+                res.append(word[0])
+                res.append(word[1:])
+            else:
+                res.append(word)
+        else:
+            res.append(word)
+    res[-1] = res[-1][:-3]  # Remove .md from the end of the file
+    return " ".join(res)
 
 
 class Model:
@@ -10,7 +28,22 @@ class Model:
 
     @memo
     def get_posts(self):
-        return json.dumps(self.data)
+        current_path = os.getcwd() + "/" + self.posts_path
+        posts = []
+        for filename in os.listdir(current_path):
+            with open(current_path + filename) as f:
+                title = []
+                desc = []
+
+                for line in f.readlines():
+                    if not line.startswith("#"):
+                        desc.append(line)
+                    else:
+                        break
+                posts.append({
+                    "title": clean_filename(filename),
+                    'desc': "".join(desc)})
+        return json.dumps(posts)
 
     @memo
     def get_post(self, key):
