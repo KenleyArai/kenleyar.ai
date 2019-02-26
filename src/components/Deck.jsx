1a6c4acab8949e6a18fd80292dcb960c959ef1a3
Kenley Arai <me@kenleyar.ai>
2019-02-25 18:49:35 -0800

    Added key to the deck UUID

--- src/components/Deck.jsx
+++ src/components/Deck.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import ListCard from 'components/ListCard';
 import DeckContainer from 'components/styled/DeckContainer';
 import PropTypes from 'prop-types';
+import uuidv1 from 'uuid/v1';
 
 const Deck = ({ posts }) => {
   return (
     <DeckContainer>
-      {posts.map(post => <ListCard key={post.toString()} {...post} />)}
+      {posts.map(post => <ListCard key={uuidv1()} {...post} />)}
     </DeckContainer>
   );
 };
